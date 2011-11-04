537c6cb34b20339b04b201920612c7569ecb472b
Terry Brown <terry_n_brown@yahoo.com>
2011-11-04 09:00:00 -0500

    working on attrib. name

--- views.py
+++ views.py
@@ -36,10 +36,61 @@ class SearchForm(forms.Form):
 
 def search(request):
     
+    total_assets = Asset.objects.count()
+    selected_count = 0
+    selected = []
+    
+    if request.method == 'POST':
+        
+        form =  SearchForm(request.POST)
+        
+        if form.is_valid():
+        
+            selection = Asset.objects.all()
+            d = form.cleaned_data
+            if d['attr_name']:
+                for i in d['attr_name'].split(','):
+                    selection = selection.filter(attribute__name=i.strip())
+                    
+            selected_count = selection.count()
+            
+            if selected_count < 101:
+                selected = [[i[0], translate_path(i[1]), i[1]]
+                    for i in selection.values_list('pk', 'path__path_txt')]
+    else:
+        
+        form = SearchForm()
+    
     return render_to_response(
         "gis_asset/search.html",
-        {
-            'form': SearchForm(),
+        {   'total_assets': total_assets,
+            'selected_count': selected_count,
+            'form': form,
+            'selected': selected,
+        },
+        RequestContext(request),
+    )
+
+def translate_path(path):
+    
+    path = path.strip('/').split('/')
+    
+    if path[0] == 'nrgisl':
+        path[0] = 'file:///N:'
+    else:
+        path.pop(0)
+        path[0] = 'file:///%s:' % path[0][0].upper()
+        
+    return '/'.join(path[:-1])
+def asset(request, pk):
+    
+    asset = Asset.objects.get(pk=pk)
+    
+    
+
+    return render_to_response(
+        "gis_asset/asset.html",
+        {   'asset': asset,
         },
         RequestContext(request),
     )
