9b415823ab56358831596fadc5bca1c159064bbe
Terry Brown <terry_n_brown@yahoo.com>
2016-04-07 16:50:09 -0500

    late commit

---
+++ migrations/0005_auto__add_field_bounds_cellsx__add_field_bounds_cellsy__add_field_boun.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Bounds.cellsx'
+        db.add_column('bounds', 'cellsx',
+                      self.gf('django.db.models.fields.IntegerField')(null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Bounds.cellsy'
+        db.add_column('bounds', 'cellsy',
+                      self.gf('django.db.models.fields.IntegerField')(null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Bounds.sizex'
+        db.add_column('bounds', 'sizex',
+                      self.gf('django.db.models.fields.FloatField')(null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Bounds.sizey'
+        db.add_column('bounds', 'sizey',
+                      self.gf('django.db.models.fields.FloatField')(null=True, blank=True),
+                      keep_default=False)
+
+
+        # Changing field 'Bounds.srid'
+        db.alter_column('bounds', 'srid', self.gf('django.db.models.fields.IntegerField')(null=True))
+
+    def backwards(self, orm):
+        # Deleting field 'Bounds.cellsx'
+        db.delete_column('bounds', 'cellsx')
+
+        # Deleting field 'Bounds.cellsy'
+        db.delete_column('bounds', 'cellsy')
+
+        # Deleting field 'Bounds.sizex'
+        db.delete_column('bounds', 'sizex')
+
+        # Deleting field 'Bounds.sizey'
+        db.delete_column('bounds', 'sizey')
+
+
+        # Changing field 'Bounds.srid'
+        db.alter_column('bounds', 'srid', self.gf('django.db.models.fields.IntegerField')(default=-1))
+
+    models = {
+        'gis_asset.asset': {
+            'Meta': {'object_name': 'Asset', 'db_table': "'asset'"},
+            'asset': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'format': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['gis_asset.Format']", 'symmetrical': 'False'}),
+            'geom_type': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['gis_asset.Geom_type']", 'symmetrical': 'False'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '4096'}),
+            'records': ('django.db.models.fields.IntegerField', [], {})
+        },
+        'gis_asset.attr_type': {
+            'Meta': {'object_name': 'Attr_type', 'db_table': "'attr_type'"},
+            'attr_type': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '4096'})
+        },
+        'gis_asset.attribute': {
+            'Meta': {'object_name': 'Attribute', 'db_table': "'attribute'"},
+            'asset': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['gis_asset.Asset']", 'db_column': "'asset'"}),
+            'attr_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['gis_asset.Attr_type']", 'db_column': "'attr_type'"}),
+            'attribute': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '4096'})
+        },
+        'gis_asset.bounds': {
+            'Meta': {'object_name': 'Bounds', 'db_table': "'bounds'"},
+            'asset': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['gis_asset.Asset']", 'db_column': "'asset'"}),
+            'bounds': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'cellsx': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'cellsy': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'maxx': ('django.db.models.fields.FloatField', [], {}),
+            'maxy': ('django.db.models.fields.FloatField', [], {}),
+            'minx': ('django.db.models.fields.FloatField', [], {}),
+            'miny': ('django.db.models.fields.FloatField', [], {}),
+            'sizex': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'sizey': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'srid': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'gis_asset.drive': {
+            'Meta': {'object_name': 'Drive', 'db_table': "'drive'"},
+            'drive': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip': ('django.db.models.fields.CharField', [], {'max_length': '4096', 'blank': 'True'}),
+            'last_scanned': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'letter': ('django.db.models.fields.CharField', [], {'max_length': '4096'}),
+            'machine': ('django.db.models.fields.CharField', [], {'max_length': '4096'}),
+            'share': ('django.db.models.fields.CharField', [], {'max_length': '4096'}),
+            'user': ('django.db.models.fields.CharField', [], {'max_length': '4096', 'blank': 'True'})
+        },
+        'gis_asset.format': {
+            'Meta': {'object_name': 'Format', 'db_table': "'format'"},
+            'format': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '4096'})
+        },
+        'gis_asset.geom_type': {
+            'Meta': {'object_name': 'Geom_type', 'db_table': "'geom_type'"},
+            'geom_type': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '4096'})
+        },
+        'gis_asset.path': {
+            'Meta': {'object_name': 'Path', 'db_table': "'path'"},
+            'asset': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['gis_asset.Asset']", 'db_column': "'asset'"}),
+            'path': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'path_txt': ('django.db.models.fields.CharField', [], {'max_length': '4096'})
+        }
+    }
+
+    complete_apps = ['gis_asset']
